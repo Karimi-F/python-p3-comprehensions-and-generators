3df08610fc760b5ba50a178d1ae39bbb6b050485
Karimi-F <fridah37karimi@gmail.com>
2024-11-28 22:33:20 +0300

    Creates lists

--- lib/list_comprehension.py
+++ lib/list_comprehension.py
@@ -1,7 +1,12 @@
 #!/usr/bin/env python3
 
 def return_evens(num_list):
-    pass
+    num_list = [num for num in num_list if num % 2 == 0]
+    return(num_list)
+print(return_evens([1,2,3,4,5]))
 
 def make_exclamation(sentence_list):
-    pass
+    sentence_list = [sentence + "!" for sentence in sentence_list if type(sentence) == str ]
+    return(sentence_list)
+
+print(make_exclamation(["Hello","I'm doing great","Python is fun"]))    
